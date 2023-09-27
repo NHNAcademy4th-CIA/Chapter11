3c5eeb2cb061c6b9bf57de9ce36ea3e663f5f66e
masiljangajji <hjk172262@gmail.com>
2023-09-27 14:14:46 +0900

    feat: Main Class 추가

---
+++ src/main/java/org/nhnacademy/lsj/Main.java
@@ -0,0 +1,35 @@
+package org.nhnacademy.lsj;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        //Problem1.problem1();
+        //problem2.problem2();
+
+
+        //Problem5.problem5();
+
+
+
+
+
+
+
+    }
+
+}
