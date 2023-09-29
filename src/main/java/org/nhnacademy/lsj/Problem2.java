38b08e3536c789bbac054667759a112e223cde1c
masiljangajji <hjk172262@gmail.com>
2023-09-29 17:38:36 +0900

    feat: file OutputStream , Using BufferWriter , PrintWriter

--- src/main/java/org/nhnacademy/lsj/Problem2.java
+++ src/main/java/org/nhnacademy/lsj/Problem2.java
@@ -6,15 +6,22 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 텍스트파일을 읽고 , 구성된 줄 수를 출력한 후 , 복사해 다른 파일로 저장한다.
+ */
 public class Problem2 {
 
     private static final Logger logger = LoggerFactory.getLogger(Problem2.class);
 
+    /**
+     * 프로그램이 시작되는 main역할.
+     */
     public static void problem2() {
 
 
@@ -27,8 +34,12 @@ public class Problem2 {
 
         for (int i = 0; i < textName.size(); i++) {
 
-            try (BufferedReader bf = new BufferedReader(new FileReader(textName.get(i)));
-                 BufferedWriter bw = new BufferedWriter(new FileWriter(textName.get(i) + "_it's a copy"))) {
+            try (BufferedReader bf = new BufferedReader(new FileReader(textName.get(i)))) {
+
+
+                BufferedWriter bw = new BufferedWriter(new FileWriter(textName.get(i) + "_Copy_By_BufferWriter"));
+                PrintWriter pw = new PrintWriter(new FileWriter(textName.get(i) + "_Copy_By_PrintWriter"),true);
+
 
                 int count = 0;
 
@@ -37,8 +48,11 @@ public class Problem2 {
                 while ((str = bf.readLine()) != null) {
                     bw.write(str);
                     bw.newLine();
+                    pw.println(str);
                     count++;
                 }
+
+                bw.flush();
 
                 logger.info("{} 파일은 {}줄로 이루어져 있습니다.", textName.get(i), count);
 
@@ -50,6 +64,8 @@ public class Problem2 {
 
 
         }
+
+
     }
 
 }
