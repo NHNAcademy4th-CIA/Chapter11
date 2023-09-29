843f4d9a52fdf0a3d99bebd147b5ce6acca1d8b0
minm063 <jangmin063@gmail.com>
2023-09-28 22:58:44 -0700

    refactor : 파일마다 줄 수를 출력하도록 수정

--- src/main/java/org/nhnacademy/minju/LineCounts.java
+++ src/main/java/org/nhnacademy/minju/LineCounts.java
@@ -3,11 +3,14 @@ package org.nhnacademy.minju;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * .입력된 텍스트 파일에 대해 줄 수를 세고 총 합을 출력한다.
  */
 public class LineCounts {
+    private static final Logger logger = LoggerFactory.getLogger(LineCounts.class);
 
     /**
      * .CLI로 입력 받으면 파라미터는 args에 저장된다. 여기서 args는 각 텍스트 파일의 이름이다.
@@ -19,7 +22,7 @@ public class LineCounts {
     public static void main(String[] args) {
         int count = 0;
         if (args.length == 0) {
-            System.out.println("한 개 이상의 파일이 입력되야 합니다.");
+            logger.warn("한 개 이상의 파일이 입력되야 합니다.");
             return;
         }
         for (int i = 0; i < args.length; i++) {
@@ -27,10 +30,11 @@ public class LineCounts {
                 while (bufferedReader.readLine() != null) {
                     count++;
                 }
+            logger.info("{} : {}줄", bufferedReader, count);
             } catch (IOException e) {
-                System.out.println(e.getMessage());
+                logger.warn(e.getMessage());
             }
         }
-        System.out.println("count : " + count);
+        logger.info("총 {}줄", count);
     }
 }
